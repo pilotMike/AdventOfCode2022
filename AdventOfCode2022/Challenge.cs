eb7a9ba8e9afa76fc789f983f4c20b695e455416
Michael DiLeo <m_dileosignup@live.com>
2022-12-01 17:40:35 -0600

    Add project files.

---
+++ AdventOfCode2022/Challenge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AdventOfCodeTests")]
+
+namespace AdventOfCode2022
+{
+    internal abstract class Challenge
+    {
+        public abstract Input DefaultInput { get; set; }
+
+
+        public string Execute(Input? input)
+        {
+            return this.ExecuteInternal(input ?? DefaultInput);
+        }
+
+        protected abstract string ExecuteInternal(Input input);
+    }
+}
---
+++ AdventOfCode2022/Input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Input
+    {
+        private readonly string lines;
+
+        public Input(string lines)
+        {
+            this.lines = lines;
+        }
+    }
+}
